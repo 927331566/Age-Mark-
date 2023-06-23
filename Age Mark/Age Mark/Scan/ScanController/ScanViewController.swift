a571e4c3a345d5f182b85c4e4abd04af904db43f
Twentix <95166340+927331566@users.noreply.github.com>
2023-06-23 17:18:32 +0800

    111

--- Age Mark/Age Mark/Scan/ScanController/ScanViewController.swift
+++ Age Mark/Age Mark/Scan/ScanController/ScanViewController.swift
@@ -11,14 +11,34 @@ class ScanViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //添加view
-        let view = ScanView()
-        view .initView()
-        self.view.addSubview(view)
+        self.view.backgroundColor = UIColor.white
+        
+        let alertController = UIAlertController(title: "Age Mark 需要权限使用相机", message: "访问您的相机，以便您进行建筑识别，隔空涂鸦，发布打卡等", preferredStyle: .alert)
+
+        // 添加操作按钮
+        let okAction = UIAlertAction(title: "Ok", style: .default) { (_) in
+            self.JumpToNext()
+        }
+        alertController.addAction(okAction)
+
+        // 添加取消按钮
+        let cancelAction = UIAlertAction(title: "Don't Allow", style: .cancel) { (_) in
+            // 取消按钮点击后执行的代码
+        }
+        alertController.addAction(cancelAction)
+
+        // 显示弹出框
+        self.present(alertController, animated: true, completion: nil)
+
+        
         // Do any additional setup after loading the view.
     }
-    
-
+    func JumpToNext() {
+            let viewController = ScanViewControllerTwo()
+            viewController.modalPresentationStyle = UIModalPresentationStyle.fullScreen
+            viewController.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+            self.present(viewController, animated: false)
+    }
     /*
     // MARK: - Navigation
 
--- Age Mark/Age Mark/Scan/ScanView/ScanView.swift
+++ Age Mark/Age Mark/Scan/ScanView/ScanView.swift
@@ -17,13 +17,40 @@ class ScanView: UIView {
     func initView() {
         self.backgroundColor = UIColor.yellow
         self.frame = CGRect(x: 0, y: 0, width: Width , height: Height)
+        
+        
+        
+        
+//        
+//        let alertController = UIAlertController(title: "Age Mark 需要权限使用相机", message: "访问您的相机，以便您进行建筑识别，隔空涂鸦，发布打卡等", preferredStyle: .alert)
+//
+//        // 添加操作按钮
+//        let okAction = UIAlertAction(title: "Don't Allow", style: .default) { (_) in
+//            // 确定按钮点击后执行的代码
+//        }
+//        alertController.addAction(okAction)
+//
+//        // 添加取消按钮
+//        let cancelAction = UIAlertAction(title: "Ok", style: .cancel) { (_) in
+//            // 取消按钮点击后执行的代码
+//        }
+//        alertController.addAction(cancelAction)
+        
+        // 显示弹出框
+        //self.presen
+        //self.addSubview(alertController)
+        
+        //    let alertController = UIAlertController(title: "Age Mark 需要权限使用相机", message: "访问您的相机，以便您进行建筑识别，隔空涂鸦，发布打卡等", preferredStyle: .alert)
+        //    let alertAction1 = UIAlertAction(title: "Don't Allow", style: .default)
+        //    let alertAction2 = UIAlertAction(title: "Ok", style: .default)
+        //    s
+        /*
+         // Only override draw() if you perform custom drawing.
+         // An empty implementation adversely affects performance during animation.
+         override func draw(_ rect: CGRect) {
+         // Drawing code
+         }
+         */
+        
     }
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
